2e34811f8db12e67e9a1711f49e95d95cde6c94e
Luca Vaccaro <luck87@gmail.com>
2017-05-27 01:57:47 +0200

    Gulp conversion es6 to es5; replace js folder location

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,20 @@ const order = require('gulp-order');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const babili = require('gulp-babili');
-var exec = require('gulp-exec');
+const connect = require('gulp-connect');
+const clean = require('gulp-clean');
+const browserify = require("browserify");
+const babelify = require("babelify");
+const source = require('vinyl-source-stream');
+const runSequence = require('run-sequence');
+
+gulp.task('clean', function () {
+    return gulp.src('assets/stylesheets/*.css', {read: false})
+        .pipe(addsrc('assets/javascripts/application.js'))
+        .pipe(addsrc('assets/javascripts/index.bundle.js'))
+        .pipe(addsrc('assets/javascripts/info.bundle.js'))
+        .pipe(clean({force: true}))
+});
 
 gulp.task('sass', function() {
   return gulp.src(['assets/stylesheets/application.scss', 'assets/stylesheets/certificate.scss','assets/stylesheets/timestamp-of.scss'])
@@ -31,7 +44,7 @@ gulp.task('compress', function() {
       }))
     .pipe(gulp.dest('assets/javascripts'))
 });
-
+/*
 gulp.task('browserify', function() {
     var options = {
         continueOnError: false, // default = false, true means don't emit error event
@@ -46,22 +59,54 @@ gulp.task('browserify', function() {
     return gulp.src('./')
         .pipe(exec('browserify -r javascript-opentimestamps script.js -o assets/javascripts/bundle.js', options))
         .pipe(exec.reporter(reportOptions));
+});*/
+
+gulp.task('index', function() {
+    return browserify({ debug: true })
+        .transform(babelify)
+        .require("assets/javascripts/page/index.js", { entry: true })
+        .bundle()
+        .pipe(source('index.bundle.js'))
+        .pipe(gulp.dest('assets/javascripts'));
+});
+
+gulp.task('info', function() {
+    return browserify({ debug: true })
+        .transform(babelify)
+        .require("assets/javascripts/page/info.js", { entry: true })
+        .bundle()
+        .pipe(source('info.bundle.js'))
+        .pipe(gulp.dest('assets/javascripts'));
 });
 
 gulp.task('javascript', function() {
-  return gulp.src('assets/javascripts/application/*.js')
-    .pipe(addsrc('assets/javascripts/vendor/index.js'))
-    .pipe(order([
-      "assets/javascripts/vendor/index.js",
-      "assets/javascripts/application/*.js"
-    ], {base: '.'}))
-    .pipe(include())
-    .pipe(concat('application.js'))
-    // .pipe(uglify())
-    .pipe(gulp.dest('assets/javascripts'));
+    return gulp.src('assets/javascripts/application/*.js')
+        .pipe(addsrc('assets/javascripts/vendor/index.js'))
+        .pipe(order([
+            "assets/javascripts/vendor/index.js",
+            "assets/javascripts/application/*.js"
+        ], {base: '.'}))
+        .pipe(include())
+        .pipe(concat('application.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/javascripts'));
+});
+
+gulp.task('default', function(done) {
+    runSequence('clean','sass','javascript', 'index', 'info', function(){
+        done();
+    });
 });
 
 gulp.task('watch', function() {
-  gulp.watch('assets/stylesheets/**/*.scss', ['sass']);
-  gulp.watch('assets/javascripts/application/*.js', ['javascript']);
+    gulp.watch('assets/stylesheets/**/*.scss', ['sass']);
+    gulp.watch('assets/javascripts/application/*.js', ['javascript']);
+    gulp.watch('assets/javascripts/page/index.js', ['index']);
+    gulp.watch('assets/javascripts/page/info.js', ['info']);
+});
+
+gulp.task('server', function() {
+    connect.server({
+        //livereload: true
+    });
 });
